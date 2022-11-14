4896d1b9049e58201741946b4daa54154608aa50
ChristineZhaoArmory <104391112+ChristineZhaoArmory@users.noreply.github.com>
2022-11-14 12:24:00 -0500

    Update mainnoprofile.tf

--- mainnoprofile.tf
+++ mainnoprofile.tf
@@ -1,1 +1,15 @@
-main.tf
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  tags = {
+    name = "TF_christine"
+  }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.example.private_ip
+}
