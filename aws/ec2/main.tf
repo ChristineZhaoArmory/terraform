3023838cd0db7ad343128e7d14bcf5ea58ac2268
ChristineZhaoArmory <104391112+ChristineZhaoArmory@users.noreply.github.com>
2022-11-15 09:16:59 -0500

    Create main.tf

---
+++ aws/ec2/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "TF_christine"
+  }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.example.private_ip
+}
+
